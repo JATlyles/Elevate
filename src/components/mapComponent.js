2e31a8ed4e27c06e837d96e572338bee84272a3e
akinghill <Austinkinghill@gmail.com>
2021-04-27 14:15:58 -0700

    make mapComponent functional

--- src/components/mapComponent.js
+++ src/components/mapComponent.js
@@ -1,111 +1,47 @@
-// import React, {useState} from 'react';
-// import MapView, { View, Text, PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
-// import {styles} from './style';
-
-// const MapComponent = () => {
-
-//   const [markers, setMarkers] = useState([]);
-
-//    const handlePress =(e) => {
-//       setMarkers({
-//         markers: [
-//           ...markers,
-//           {
-//             coordinate: e.nativeEvent.coordinate,
-//             elevation: 23243,
-//           }
-//         ]
-//       })
-//        }
-
-//   return(
-//     <>
-//          <MapView
-//            showsUserLocation
-//            followsUserLocation
-//            // provider={PROVIDER_GOOGLE}
-//            style={styles.map}
-//            initialRegion={{
-//              latitude: 37.7896386,
-//              longitude: -122.421646,
-//              latitudeDelta: 0.09,
-//              longitudeDelta: 0.035,
-//            }}
-//            onPress={handlePress}
-//            >
-//            {markers.map((marker) =>{
-//              return (
-             
-//                <Marker {...marker} />
-//              )
-//            })}
-//          </MapView>
-//        </>
-//   )
-// }
-
-// export default MapComponent;
-
-import React, {Component} from 'react';
-import MapView, { View, Text, PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
 import {styles} from './style';
-import CustomMarker from './customMarker'
+import CustomMarker from './customMarker';
 import {ElevationContext} from '../Contexts/elevationContext';
+import {useContext, useState} from 'react';
 
-export default class MapComponent extends Component {
-  constructor(props) {
-    super(props);
+export default function MapComponent(props) {
+  const [currentElevation] = useContext(ElevationContext);
 
-    this.state = {
-      markers: []
-    }
-    this.handlePress = this.handlePress.bind(this)
-  }
+  const [markers, setMarkers] = useState([]);
 
-  handlePress(e) {
-    this.setState({
-      markers: [
-        ...this.state.markers,
-        {
-          coordinate: e.nativeEvent.coordinate,
-          key: Math.round(Math.random()* 10000000000)
-        }
-      ]
-    })
-  }
+  const handlePress = e => {
+    setMarkers([
+      ...markers,
+      {
+        coordinate: e.nativeEvent.coordinate,
+        key: Math.round(Math.random() * 10000000000),
+      },
+    ]);
+  };
 
-  render() {
-    return (
-      <>
-        <MapView
-          showsUserLocation
-          followsUserLocation
-          // provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          initialRegion={{
-            latitude: 35.0997,
-            longitude: -90.0108,
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.035,
-          }}
-          onPress={this.handlePress}
-          >
-          {this.state.markers.map((marker) =>{
-            return (
-             
-              <Marker  key={marker.key} {...marker} >
-              <CustomMarker/>
-              </Marker>
-            )
-          })}
-        </MapView>
-      </>
-    );
-  }
+  return (
+    <>
+      <MapView
+        showsUserLocation
+        followsUserLocation
+        // provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 35.0997,
+          longitude: -90.0108,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.035,
+        }}
+        onPress={handlePress}>
+        {markers.map(marker => {
+          return (
+            <Marker key={marker.key} {...marker}>
+              <CustomMarker elevation={currentElevation} />
+            </Marker>
+          );
+        })}
+      </MapView>
+    </>
+  );
 }
-
-  {/* <Marker {...marker} >
-                <View style={styles.marker}>
-                  <Text style={styles.text}>{marker.elevation}</Text>
-                </View>
-              </Marker> */}
