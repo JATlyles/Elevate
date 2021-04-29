959592a7385a6ed70c0ce6757c63fdeeec766cd6
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-04-29 13:33:50 -0500

    my bad this is theone

--- src/components/placePicker.js
+++ src/components/placePicker.js
@@ -1,60 +1,77 @@
-import React from 'react';
-import MapPicker from "react-native-map-picker";
-import { View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 
-export default class SelectLocationScreen extends React.Component {
-    state = {
-        chosenLatitude: 0,
-        chosenLongitude: 0,
-        chosenElevation: 0,
+
+class Maps extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            region: {
+                latitude: 24.92009056750823,
+                longitude: 67.1012272143364,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1
+            },
+
+            marker: null
+        }
     }
 
-    findElevation() {
+    onMarkerCalloutPress = () => {
         let elevateCoordinates = this.state.markerLat + "," + this.state.markerLon;
-        let elevationCoordinates = elevateCoordinates.toString();
+        let elevationCoordinates = elevCoordinates.toString();
         console.log(elevationCoordinates);
-        const res = fetch(`https://maps.googleapis.com/maps/api/elevation/json?locations=${elevationCoordinates}&key=AIzaSyD3pCgrdlCaWjT_AIe13jaeKf4zfpGK8R4`)
-            .then(response => response.json())
-            .then(json => console.log(json))
-        const data = res.json()
-        const currentChosenElevation = JSON.stringify(data.results[0].elevation);
-        this.setState({
-            chosenElevation: currentChosenElevation,
-        });
-    }
-    onMapPress(e) {
-        alert("coordinates:" + JSON.stringify(e.nativeEvent.coordinate));
-
-        this.setState({
-            marker: [
-                {
-                    coordinate: e.nativeEvent.coordinate
-                }
-            ]
-        });
-    }
-    setLocation() {
-        this.setState({
-            chosenLatitude: latitude,
-            chosenLongitude: longitude,
-        });
+        return fetch(`https://maps.googleapis.com/maps/api/elevation/json?locations=${elevationCoordinates}&key=AIzaSyD3pCgrdlCaWjT_AIe13jaeKf4zfpGK8R4`)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson.results.elevation),
+                    this.setState({ markerElevation: responseJson.results.elevation });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
 
     }
-
     render() {
 
         return (
-            <View style={{ flex: 1 }}>
-                <MapPicker
-                    initialCoordinate={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                    }}
-                    onLocationSelect={({ latitude, longitude }) =>
-                        console.log(longitude, latitude)}
-                />
-            </View>
+
+            <MapView style={styles.map} region={this.state.region}
+                onPress={(e) =>
+
+                    this.setState({
+                        markerLat: e.nativeEvent.coordinate.latitude,
+                        markerLon: e.nativeEvent.coordinate.longitude,
+                        markerAlt: e.nativeEvent.coordinate.altitude,
+                        marker: e.nativeEvent.coordinate
+                    })}>
+                {
+                    this.state.marker &&
+                    <MapView.Marker coordinate={this.state.marker} >
+                        <Callout onPress={this.onMarkerCalloutPress}>
+                            <Text>Lat: {this.state.markerLat}</Text>
+                            <Text>Lon: {this.state.markerLon}</Text>
+
+                        </Callout>
+
+                    </MapView.Marker>
+                }
+            </MapView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject
+    }
+});
