9c8849465213702a7a2d64e1c43fc490367ff0ef
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-04-29 15:10:48 -0500

    this is it, had to make it one solid var and turn it into string

--- src/components/placePicker.js
+++ src/components/placePicker.js
@@ -3,22 +3,25 @@ import { View, Text, StyleSheet, Alert } from "react-native";
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 
-
-class Maps extends React.Component {
+// const elevator = new google.maps.ElevationService();
+export default class App extends React.Component {
     constructor(props) {
         super(props)
-
+        this.onMarkerCalloutPress = this.onMarkerCalloutPress.bind(this);
         this.state = {
-            region: {
-                latitude: 24.92009056750823,
-                longitude: 67.1012272143364,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1
-            },
 
-            marker: null
+            markerLat: 0,
+            markerLon: 0,
+            latitude: 24.92009056750823,
+            longitude: 67.1012272143364,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+            eleCoordinates: 0,
+            markerElevation: 0,
+            marker: null,
         }
     }
+
 
     onMarkerCalloutPress = () => {
         let elevateCoordinates = this.state.markerLat + "," + this.state.markerLon;
@@ -27,34 +30,33 @@ class Maps extends React.Component {
         return fetch(`https://maps.googleapis.com/maps/api/elevation/json?locations=${elevationCoordinates}&key=AIzaSyD3pCgrdlCaWjT_AIe13jaeKf4zfpGK8R4`)
             .then((response) => response.json())
             .then((responseJson) => {
-                console.log(responseJson.results.elevation),
-                    this.setState({ markerElevation: responseJson.results.elevation });
+                console.log(responseJson.results[0].elevation),
+                    this.setState({ markerElevation: responseJson.results[0].elevation });
             })
             .catch((error) => {
                 console.error(error);
             });
 
     }
+
+
     render() {
-
+        const elevationMarkerCoordinates = this.state.eleCoordinates;
         return (
-
             <MapView style={styles.map} region={this.state.region}
                 onPress={(e) =>
-
                     this.setState({
                         markerLat: e.nativeEvent.coordinate.latitude,
                         markerLon: e.nativeEvent.coordinate.longitude,
-                        markerAlt: e.nativeEvent.coordinate.altitude,
                         marker: e.nativeEvent.coordinate
                     })}>
                 {
                     this.state.marker &&
                     <MapView.Marker coordinate={this.state.marker} >
                         <Callout onPress={this.onMarkerCalloutPress}>
-                            <Text>Lat: {this.state.markerLat}</Text>
-                            <Text>Lon: {this.state.markerLon}</Text>
-
+                            <Text>Lat:{this.state.markerLat}</Text>
+                            <Text>lon:{this.state.markerLon}</Text>
+                            <Text>Elevation:{this.state.markerElevation}</Text>
                         </Callout>
 
                     </MapView.Marker>
