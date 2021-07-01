4de5c130e45be8d71a8e531d85c7b9e0aabe62c3
David <jlyles@banyanlabs.io>
2021-07-01 15:01:05 -0500

    made callout component

---
+++ src/components/callout/Callout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+export const Callout = () => {
+  return (
+    <View>
+      <Text>This is a Callout component</Text>
+    </View>
+  );
+};
